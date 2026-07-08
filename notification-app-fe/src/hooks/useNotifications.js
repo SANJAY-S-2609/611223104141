13a915366b114059b70a1ec94e40bf5a96cc257b
SANJAY-S-2609 <sanjaysampath2609@gmail.com>
2026-07-08 12:22:10 +0530

    use Notification hooks created succesfully

--- notification-app-fe/src/hooks/useNotifications.js
+++ notification-app-fe/src/hooks/useNotifications.js
@@ -1,20 +1,52 @@
 import { useState, useEffect } from "react";
-import { fetchNotifications } from "../apis/notifications";
+import { fetchNotifications } from "../api/notifications";
 
 export function useNotifications() {
   const [notifications, setNotifications] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const [filter, setFilter] = useState("All");
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const load = async () => {
-      const data = await fetchNotifications();
-      setNotifications(data.notifications ?? []);
+      try {
+        setLoading(true);
+
+        const data = await fetchNotifications();
+
+        setNotifications(data.notifications ?? []);
+        setTotal(data.notifications?.length ?? 0);
+
+        setError(null);
+      } catch (err) {
+        setError("Failed to load");
+      } finally {
+        setLoading(false);
+      }
     };
 
     load();
-  }, [notifications]);
+  }, []);
 
-  const totalPages = 0;
+  const filteredNotifications =
+    filter === "All"
+      ? notifications
+      : notifications.filter(
+          (notification) => notification.category === filter
+        );
 
-  return { notifications, total, totalPages, loading: false, error: true };
+  const totalPages = Math.ceil(filteredNotifications.length / 5);
+
+  return {
+    notifications: filteredNotifications,
+    total,
+    totalPages,
+    loading,
+    error,
+    filter,
+    setFilter,
+  };
 }
